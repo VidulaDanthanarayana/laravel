4b8392b5a91ac8dd4f803937ea6ff4302c745b4a
VidulaDantanarayana <vidula.dantanarayana@spemai.com>
2026-05-23 16:07:34 +0530

    added app

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:k2Z8qvTyRTIbC4rve73N94CoYx8hDHqXfeVKUpVjL70='),
 
     'previous_keys' => [
         ...array_filter(
